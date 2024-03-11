a40388bb71e07632e1d086098fdfd1d8e372b229
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-03-10 23:02:17 -0300

    Inicialização do Projeto

---
+++ src/main/java/com/vinimanfrin/safeway/SafewayApplication.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.safeway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafewayApplication.class, args);
+	}
+
+}
